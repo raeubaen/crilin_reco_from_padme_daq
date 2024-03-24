9ae021250112ada9c368cfe1cedc4e2d403d6405
MU2E <mu2e@mu2edaq.lnf.infn.it>
2024-03-24 17:11:13 +0100

    onlinemonitor rewritten

--- plot_monitor.C
+++ plot_monitor.C
@@ -7,17 +7,16 @@
 int x(int cry) {return 2 - (cry/2)/3;}
 int y(int cry) {return (cry/2)%3;}
 
-void plot_monitor(TString filebig, TString filesmall){
+void plot_monitor(TString all_files, TString lastfile){
 
   gROOT->LoadMacro("/home/mu2e/rootlogon.C");
-  TFile *f[2];
-  f[0] = new TFile(filesmall);
-  f[1] = new TFile(filebig);
-  TTree *intree[2];
-  intree[0] = (TTree*)f[0]->Get("NTU");
-  intree[1] = (TTree*)f[1]->Get("NTU");
+  TChain *intree[2];
+  for (int i=0; i<2; i++) intree[i] = new TChain("NTU");
 
-  auto *c = new TCanvas("c", "Crilin H2 August 2023 - crystal charge online monitor - cyan=allrecofragments", 1700, 900);
+  intree[0]->Add(all_files);
+  intree[1]->Add(lastfile);
+
+  auto *c = new TCanvas("c", "Crilin BTF Spring 2024 - crystal charge online monitor - cyan=allrecofragments", 1700, 900);
   c->Divide(2);
 
   gStyle->SetLabelSize(0.05, "X");
@@ -26,18 +25,16 @@ void plot_monitor(TString filebig, TString filesmall){
   gStyle->SetTitleY(1.1);
   gStyle->SetStatX(0.6);
   gStyle->SetStatY(0.6);
-  
+
   for(int j=0; j<2; j++){
     auto *p = c->cd(j+1);
     p->Divide(3, 3);
-    if (j==1) p->SetFillColor(kCyan);
+    if (j==0) p->SetFillColor(kCyan);
     for(int i=0; i<9; i++){
       int _x = x(i*2);
       int _y = y(i*2);
       p->cd((3*_x+_y + 1));
-      cout << i << endl;
       intree[j]->Draw(Form("(QCh[0][%i]+QCh[0][%i])/2>>h_%i(200, 0, 1000)", i*2, i*2+1, 9*j+i),Form("QCh[0][%i]>5 && QCh[0][%i]>5",i*2, i*2+1));
-      cout << "finito" << endl;
     }
   }
   c->SaveAs("../outonline/onlinemonitorbtf_last_summary.root");
