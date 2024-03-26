1dfdc8040aa06eda96f54c84265103137d11ef91
ruben <r@g.it>
2024-03-26 12:33:29 +0100

    onthefly fixed

--- job_runmap_condor.py
+++ job_runmap_condor.py
@@ -1,5 +1,3 @@
-#INCOMPLETOOOOOOOOOOOOO
-
 import os
 import sys
 import pandas as pd
@@ -8,11 +6,6 @@ import argparse
 parser = argparse.ArgumentParser(description='Run submit for condor')
 
 parser.add_argument('mapfile', type=str, help='File with the map')
-parser.add_argument('--nevents_per_job', type=int, help='nevents_per_job', default=2000)
-parser.add_argument('--njobs', type=int, help='njobs', default=50)
-parser.add_argument('--rootinputfolder', type=str, help="Root input folder", default='/eos/user/r/rgargiul/www/crilin/input/recoruns/')
-parser.add_argument('--condorfolder', type=str, help="Folder where logs etc. are saved", default='/afs/cern.ch/work/r/rgargiul/crilin_jobs')
-parser.add_argument('--rootoutfolder', type=str, help="Root/json out folder", default='/eos/user/e/edimeco/BTF/crilin/output/')
 
 args = parser.parse_args()
 v = vars(args)
@@ -20,12 +13,12 @@ print(v)
 vars().update(v)
 
 def reco(row):
-  cmd = f"python3 submit_condor.py {row.nrun} {row.nrun} {row.board} {int(row.timeoffset)} {nevents_per_job} {njobs} --rootinputfolder {rootinputfolder} --condorfolder {condorfolder} --rootoutfolder {rootoutfolder}"
+  cmd = f"python3 submit_condor_btf_2024.py {row.nrun} {row.label}"
   print(cmd)
   os.system(cmd)
 
 df = pd.read_csv(mapfile, sep=" ", header=None)
-df.columns = ["nrun", "board", "timeoffset"]
+df.columns = ["nrun", "label"]
 
 print(df)
 
--- script_charge_onthefly.py
+++ script_charge_onthefly.py
@@ -1,23 +1,28 @@
 import glob
 import ROOT
+import os
 
 folders = glob.glob("/eos/user/e/edimeco/BTF/crilin/output/*")
 
 for folder in folders:
-  print(folder)
   try:
     ncry = int(folder.split("cry")[1])
+    label = folder.split("/")[-1].split("cry")[0].split("_")[1]
   except IndexError:
     continue
+  print(ncry, label)
   c = ROOT.TChain("tree")
   c.Add(f"{folder}/*.root")
-  h = ROOT.TH1F(f"h_{ncry}", f"charge_cry{ncry}", 100, 5, 105)
+  h = ROOT.TH1F(f"h_{ncry}_{label}", f"charge_cry{ncry}_{label}", 100, 5, 105)
   cnv = ROOT.TCanvas()
-  c.Draw(f"(charge[0][{ncry*2}]+charge[0][{ncry*2+1}])/2>>h_{ncry}")
+  c.Draw(f"(charge[0][{ncry*2}]+charge[0][{ncry*2+1}])/2>>h_{ncry}_{label}")
   print(h.Integral(), h.GetMean())
   f = ROOT.TF1("func", "gaus(0)+gaus(3)", 10, 65);
   f.SetParameters(500, 25, 10, 50, 50, 7);
+  if label=="87V": f.SetParameters(500, 17, 10, 30, 50, 7);
   h.Fit(f, "R")
-  h.SetTitle(f"Cry: {ncry} - Mean Charge: {f.GetParameter(1)} +/- {f.GetParError(1)} pC")
-  cnv.SaveAs(f"../plots/cnv_{ncry}.root")
-  cnv.SaveAs(f"../plots/cnv_{ncry}.pdf")
+  h.SetTitle(f"{label} - Cry: {ncry} - Mean Charge: {f.GetParameter(1)} +/- {f.GetParError(1)} pC")
+  cnv.SaveAs(f"../plots/cnv_{label}_{ncry}.root")
+  cnv.SaveAs(f"../plots/cnv_{label}_{ncry}.pdf")
+
+os.system("pdfunite ../plots/*.pdf all.pdf")
