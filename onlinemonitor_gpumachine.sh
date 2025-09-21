2cf71461e86697fbe846feaff482596b48167ac3
root <root@crilin.cern.ch>
2025-09-22 01:52:09 +0200

    bugfix gpu side

--- onlinemonitor_gpumachine.sh
+++ onlinemonitor_gpumachine.sh
@@ -4,19 +4,22 @@
 
 #lastfile=$(ls -1 local/rawdata/$folder | sort -V | tail -n 2 | head -n 1)
 
-xrdcp -f "root://eospublic.cern.ch//eos/experiment/muoncollider/data/crilin/h2-2025/last_file.txt" /tmp/last_file.txt
-
 orig=$(pwd)
 
 while true; do
 
-        echo cycling
+        sleep 0.1
+        xrdcp -f "root://eospublic.cern.ch//eos/experiment/muoncollider/data/crilin/h2-2025/last_file.txt" /tmp/last_file.txt
+
+        echo "last: $(cat /tmp/last_file.txt), current: $(cat /tmp/current_file.txt)"
 
 	if [ ! -e /tmp/current_file.txt ]; then
-	    cp -n /tmp/last_file.txt /tmp/current_file.txt;
+	    /bin/cp /tmp/last_file.txt /tmp/current_file.txt;
 	elif ! cmp -s /tmp/last_file.txt /tmp/current_file.txt; then
-	    cp -n /tmp/last_file.txt /tmp/current_file.txt;
+            echo "è cambiato..."
+	    /bin/cp /tmp/last_file.txt /tmp/current_file.txt;
 	else
+            echo "non è cambiato"
 	    continue
         fi
 
@@ -30,19 +33,20 @@ while true; do
 
 	echo "--------------- runname,fragment: $run_name, $n_fragment"
 
-	unpacked_xrd="root://eospublic.cern.ch/$filename"
-	reco_xrd="root://eospublic.cern.ch//eos/experiment/muoncollider/data/crilin/h2-2025/gpu_reco_files/reco_${run_name}_${n_fragment}.root"
-
-	mkdir -p /root/reco_files/${run_name}
-
-	#source /cvmfs/sft.cern.ch/lcg/views/LCG_107/x86_64-el9-gcc11-opt/setup.sh
-
-	mkdir -p /root/plot_files/$run_name/current_fragment/
-
-        echo "----------------------- /root/plot_files/$run_name/current_fragment/ -----------------------"
-	python3 reco_gpu.py -ro /root/reco_files/$run_name/reco_$n_fragment.root -i $filename -po /root/plot_files/$run_name/current_fragment/ -dj /root/crilin_reco_from_padme_daq/confs/$conf.json -hd "source /root/crilin_reco_from_padme_daq/hadd_gpu.sh $run_name $conf $n_fragment &"
-
-	xrdcp -f /root/reco_files/$run_name/reco_$n_fragment.root $reco_xrd &
+#
+#	unpacked_xrd="root://eospublic.cern.ch/$filename"
+#	reco_xrd="root://eospublic.cern.ch//eos/experiment/muoncollider/data/crilin/h2-2025/gpu_reco_files/reco_${run_name}_${n_fragment}.root"
+#
+#	mkdir -p /root/reco_files/${run_name}
+#
+#	#source /cvmfs/sft.cern.ch/lcg/views/LCG_107/x86_64-el9-gcc11-opt/setup.sh
+#
+#	mkdir -p /root/plot_files/$run_name/current_fragment/
+#
+#        echo "----------------------- /root/plot_files/$run_name/current_fragment/ -----------------------"
+#	python3 reco_gpu.py -ro /root/reco_files/$run_name/reco_$n_fragment.root -i $filename -po /root/plot_files/$run_name/current_fragment/ -dj /root/crilin_reco_from_padme_daq/confs/$conf.json -hd "source /root/crilin_reco_from_padme_daq/hadd_gpu.sh $run_name $conf $n_fragment &"
+#
+#	xrdcp -f /root/reco_files/$run_name/reco_$n_fragment.root $reco_xrd &
 
 done
 
