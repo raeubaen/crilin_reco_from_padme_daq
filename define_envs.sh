d9b97f8acc6385442137cca7aae72af3dd5b79a9
root <root@crilin.cern.ch>
2026-06-18 19:08:04 +0200

    define envs preliminary

---
+++ define_envs.sh
@@ -0,0 +1,18 @@
+export USE_CUDA=1
+
+export HOME_DIR="/root/"
+
+export PLOT_MAIN_FOLDER="/eos/user/r/rgargiul/www/h2dqm/CRILIN_TB_2026-dev/"
+export RECO_UNPACKED_OUTDIR="/eos/experiment/muoncollider/data/crilin/h2-2026/"
+export UNPACKED_FOLDER="${RECO_UNPACKED_OUTDIR}/DataTree_dqm"
+export RECO_FOLDER="${RECO_UNPACKED_OUTDIR}/reco_dqm"
+
+export LOGS_DIR="eos/user/r/rgargiul/www/h2dqm/CRILIN_TB_2026-dev/logs/"
+
+export WORKING_DIR="${HOME_DIR}/crilin_reco_from_padme_daq"
+export JSON_CONF="${HOME_DIR}/crilin_reco_from_padme_daq/confs/detectors_conf.json"
+export PHP_FILES_DIR="${HOME_DIR}/crilin_reco_from_padme_daq/ferrari_core/php/"
+export PLOTS_PLUGINS_FOLDER="${HOME_DIR}/crilin_reco_from_padme_daq/custom_plot_functions/"
+
+export HADD_NOW_DIRS="${PLOT_MAIN_FOLDER}/to_hadd_now.txt"
+export HADD_GLOB_BUFFER="${PLOT_MAIN_FOLDER}/to_hadd_buffer.txt"
