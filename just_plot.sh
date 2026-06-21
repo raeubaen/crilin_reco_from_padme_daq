614ff560a7c64dcadb06331506bdeddba4259751
root <root@crilin.cern.ch>
2026-06-21 15:48:24 +0200

    spill -> fragment and fragment name refactoring

--- just_plot.sh
+++ just_plot.sh
@@ -2,23 +2,23 @@
 
 # --- launch settings with beam|laser as input parameter ---
 if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <run_number> <spill_number> <spill_type>"
+    echo "Usage: $0 <run_number> <fragment_number> <fragment_type>"
     return
 fi
+
 RUN=$1
-SPILL=$(printf "%04d" $((10#$2)))
+FRAGMENT=$2
 OPTION=$3
 
 echo run: $RUN
-echo spill: $SPILL
-echo SPILL_TYPE: $OPTION
+echo fragment: $FRAGMENT
+echo FRAGMENT_TYPE: $OPTION
 
-SPILL_STR="${SPILL}_${OPTION}"
+FRAGMENT_STR="${FRAGMENT}_${OPTION}"
 
 mkdir ${PLOT_MAIN_FOLDER}/run_$RUN/
 
-RECO_FILE=${RECO_UNPACKED_OUTDIR}/reco_dqm/run_$RUN/${RUN}_${SPILL}_reco.root
-PLOT_SPILL_FOLDER=${PLOT_MAIN_FOLDER}/run_$RUN/spill_${SPILL_STR}
+RECO_FILE=${RECO_FOLDER}/run_$RUN/${RUN}_${FRAGMENT}_reco.root
+PLOT_FRAGMENT_FOLDER=${PLOT_MAIN_FOLDER}/run_$RUN/fragment_${FRAGMENT_STR}
 
-python3 -m ferrari_core.offline-scripts.plot_uproot_numpysyntax -i ${RECO_FILE} -po ${PLOT_SPILL_FOLDER} -j ${JSON_CONF} -opt $OPTION
-
+python3 -m ferrari_core.offline-scripts.plot_uproot_numpysyntax -i ${RECO_FILE} -po ${PLOT_FRAGMENT_FOLDER} -j ${JSON_CONF} -opt $OPTION
