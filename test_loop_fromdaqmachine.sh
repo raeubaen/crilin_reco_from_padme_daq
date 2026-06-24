03b61ea06e846d37b868cc970393bb0b10881ef5
crilin <crilin@crilin-daq.dyndns.cern.ch>
2026-06-24 21:29:36 +0200

    parametrized

--- test_loop_fromdaqmachine.sh
+++ test_loop_fromdaqmachine.sh
@@ -1,7 +1,7 @@
-mkdir ${DAQ_MACHINE_UNPACKED_FOLDER}/run_0000342_20260618_151946
+mkdir ${DAQ_MACHINE_UNPACKED_FOLDER}/run_${TEST_RUN}
 
-for i in $(seq 0 100); do
+for i in $(seq 1 100); do
   echo "writing $i"
-  touch ${DAQ_MACHINE_UNPACKED_FOLDER}/run_0000342_20260618_151946/run_0000342_20260618_151946_lvl1_00_$(printf "%03d" $i).root
+  touch ${DAQ_MACHINE_UNPACKED_FOLDER}/run_${TEST_RUN}/run_${TEST_RUN}_lvl1_00_$(printf "%03d" $i).root
   sleep 14;
 done
