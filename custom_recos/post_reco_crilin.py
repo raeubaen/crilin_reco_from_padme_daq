04c39dccc0270bc0dada779c0ab026141906345a
root <root@crilin.cern.ch>
2026-06-28 14:19:22 +0200

    bugfix

--- custom_recos/post_reco_crilin.py
+++ custom_recos/post_reco_crilin.py
@@ -1,12 +1,13 @@
 from ferrari_core.registry import register_routine
 import numpy as np
 
-def centroid(fractions, w0_log_centroid, x, y):
-    w = np.maximum(0.0, w0_log_centroid + np.log(np.clip(peak_fraction, 1e-8, None)))
+
+def centroid(fractions, w0_log_centroid, x_or_y):
+    w = np.maximum(0.0, w0_log_centroid + np.log(np.clip(fractions, 1e-8, None)))
 
     w /= (np.sum(w, axis=1, keepdims=True))
 
-    return {"ix": w @ x, "iy": w @ y}
+    return w @ x_or_y
 
 
 
@@ -19,7 +20,7 @@ def post_reco_crilin(mask, reco, **kwargs):
 
   peak = reco["crilin_peak"] * peak_noise_flag
 
-  peak_sum = np.sum(peak_current_layer, axis=1)
+  peak_sum = np.sum(peak, axis=1)
 
   peak_fraction = np.zeros_like(peak)
   peak_fraction[peak_sum>0] = peak[peak_sum>0] / peak_sum[peak_sum>0][:, None]
@@ -28,8 +29,8 @@ def post_reco_crilin(mask, reco, **kwargs):
   iy_centroid = centroid(peak_fraction, w0_log_centroid, reco["crilin_iy"][0])
 
   reco.update({
-    f"crilin_peak_sum_thr_yes": peak_sum,
-    f"crilin_peak_thr_yes_central": np.sum(peak_sum*(reco["crilin_ix"]==0)*(reco["crilin_iy"]==0), axis=1),
+    f"crilin_peak_thr_yes": peak,
+    f"crilin_peak_thr_yes_sum": peak_sum,
     f"crilin_peak_thr_yes_fraction": peak_fraction}
   )
   reco.update({f"crilin_ix_centroid": ix_centroid, f"crilin_iy_centroid": iy_centroid})
@@ -50,8 +51,8 @@ def post_reco_crilin(mask, reco, **kwargs):
     reco.update({f"crilin_ix_centroid_layer_{layer}": ix_centroid_current_layer, f"crilin_iy_centroid_layer_{layer}": iy_centroid_current_layer})
 
     reco.update({
-      f"crilin_peak_sum_thr_yes_layer_{layer}": peak_current_layer_sum,
-      f"crilin_peak_thr_yes_central_layer_{layer}": np.sum(peak_current_layer_sum*(reco["crilin_ix"]==0)*(reco["crilin_iy"]==0), axis=1),
+      f"crilin_peak_thr_yes_sum_layer_{layer}": peak_current_layer_sum,
+      f"crilin_peak_thr_yes_central_layer_{layer}": np.sum(peak_current_layer*(reco["crilin_ix"]==0)*(reco["crilin_iy"]==0), axis=1),
       f"crilin_peak_thr_yes_fraction_layer_{layer}": peak_current_layer_fraction}
     )
 
